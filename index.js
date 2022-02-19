693d633ee12936db82555e29478ab91283a48222
Ayush Pote <f20190112@hyderabad.bits-pilani.ac.in>
2022-02-19 17:56:32 +0530

    add index.js file for backend

---
+++ index.js
@@ -0,0 +1,9 @@
+const cors = require('cors')
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`listening on port ${port}`))
