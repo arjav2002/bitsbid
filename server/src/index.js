27b565a71e1fc1b12aebbb44ff4042804c86a5a5
Arjav Garg <arjavgarg@gmail.com>
2022-03-03 12:15:33 +0530

    docker development setup complete

--- server/src/index.js
+++ server/src/index.js
@@ -5,5 +5,9 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })  
+
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on port ${port}`))
