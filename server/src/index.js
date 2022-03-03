6e532f0ba97e96dbe1a73f0c1551bbfa224b93a3
Arjav Garg <arjavgarg@gmail.com>
2022-03-03 12:41:24 +0530

    Connecting to db

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,16 @@ const cors = require('cors')
 const express = require('express')
 const app = express()
 const path = require('path')
+const mongoose = require('mongoose')
 
 app.use(express.json())
 app.use(cors())
+
+//connect to mongodb
+const dbURL = process.env.MONGO_URL
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(res => console.log("Connected to the database"))
+  .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/public", "index.html"))
