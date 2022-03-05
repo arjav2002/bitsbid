195d0c5c87c083efac4947cde880d33d8882fc94
aayush11shah <akscoder29@gmail.com>
2022-03-05 23:37:18 +0530

    feed shifted to home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,8 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Body from './components/Body'
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-      <Body />
-      <Footer />
-    </>
+    <div>App</div>
   )
 }
 
