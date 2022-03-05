ca0188d72b25b473321ccf522d173c15cf68d062
aayush11shah <akscoder29@gmail.com>
2022-03-05 10:12:37 +0530

    Basic NavBar Footer

---
+++ client/src/utils/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme1 = createTheme({
+    palette: {
+        primary: {
+            main: '#ececec',
+        },
+        secondary: {
+            main: '#FD5602',
+        }
+    },
+    // shape: {
+    //     borderRadius:5,
+    // },
+    
+});
+export default theme1;
